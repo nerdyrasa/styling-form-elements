65bb4d35ded7d00f1a1dcaafcf9b0e8ac9290bf6
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-12-13 11:38:26 -0600

    Change the button type from submit to button to eliminate 405 error since not submitting to server. Adjust styling. Add javascript to hide the form when the button is clicked.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function(){
+
+    "use strict";
+
+    document.getElementById("submit-btn").addEventListener("click", function(){
+      document.getElementById("signup").style.display = "none";
+      document.getElementById("thanks").style.display = "block";
+    });
+})();
